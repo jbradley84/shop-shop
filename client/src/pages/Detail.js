fa991c19f3bca1e16a41788ea89157b43e8b1bee
jbradley84 <utbradley84@yahoo.com>
2022-09-22 14:36:32 -0500

    implemented shopping cart storage with indexedDb

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -51,18 +51,26 @@ function Detail() {
   }, [products, data, dispatch, id]);
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+    const itemInCart = cart.find((cartItem) => cartItem._id === id)
+
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
       });
+      // if updating quantity, use existing item data and increment purchaseQuantity value by one
+      idbPromise('cart', 'put', {
+         ...itemInCart,
+         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...currentProduct, purchaseQuantity: 1 },
       });
+      // if product isn't in the cart yet, add it to the current shopping cart in IndexedDB
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
   };
 
@@ -71,6 +79,9 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id,
     });
+
+    // upon removal from cart, delete the item from IndexedDB using the `currentProduct._id` to locate what to remove
+    idbPromise('cart', 'delete', { ...currentProduct });
   };
 
   return (
